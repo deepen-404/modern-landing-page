2e3cabdc5447882b8d5000e9ac4753f2e3ac0e21
deepen-404 <117335002+dipen40414@users.noreply.github.com>
2023-10-13 19:24:47 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+Shery.imageEffect("#back", {
+  style: 5,
+  // debug: true,
+  config: {
+    a: { value: 1.49, range: [0, 30] },
+    b: { value: -0.98, range: [-1, 1] },
+    aspect: { value: 1.8822947576656774 },
+    gooey: { value: true },
+    infiniteGooey: { value: true },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: false },
+    maskval: { value: 1.25, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: false },
+    onMouse: { value: 1 },
+    noise_speed: { value: 1.59, range: [0, 10] },
+    metaball: { value: 0.21, range: [0, 2] },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0, range: [0, 0.1] },
+    noise_height: { value: 0.47, range: [0, 2] },
+    noise_scale: { value: 12.15, range: [0, 100] },
+  },
+  gooey: true,
+});
+
+const elements = document.querySelectorAll("#elem");
+
+elements.forEach((element) => {
+  const h1s = element.querySelectorAll("h1");
+  let index = 0;
+  let animating = false;
+  document.querySelector("#main").addEventListener("click", function () {
+    if (!animating) {
+      animating = true;
+      // moving the selected h1 out of the screen
+      gsap.to(h1s[index], {
+        top: "-=100%",
+        ease: Expo.easeInOut,
+        duration: 1,
+        onComplete: function () {
+          gsap.set(this._targets[0], {
+            top: "100%",
+          });
+          animating = false;
+        },
+      });
+
+      index === h1s.length - 1 ? (index = 0) : index++;
+
+      // making another h1 to come into the view
+      gsap.to(h1s[index], {
+        top: "-=100%",
+        ease: Expo.easeInOut,
+        duration: 1,
+      });
+    }
+  });
+});
+
+// {"a":{"value":1.49,"range":[0,30]},"b":{"value":-0.98,"range":[-1,1]},"aspect":{"value":2.2170901261055813},"gooey":{"value":true},"infiniteGooey":{"value":true},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":false},"onMouse":{"value":1},"noise_speed":{"value":1.59,"range":[0,10]},"metaball":{"value":0.21,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.47,"range":[0,2]},"noise_scale":{"value":12.15,"range":[0,100]},"maskval":{"value":1.25,"range":[1,5]}}
